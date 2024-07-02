7119263778a7c58bcbede82dc4fcd737a9ed7766
LieGeee <116617153+LieGeee@users.noreply.github.com>
2024-07-02 15:47:32 +0800

    补充括号匹配(没写完),函数包

--- calculator/src/com/cyx/Bottom/Bottom_stack_push.java
+++ calculator/src/com/cyx/Bottom/Bottom_stack_push.java
@@ -3,10 +3,10 @@ package com.cyx.Bottom;
 import java.util.Stack;
 
 public class Bottom_stack_push extends btm{
-    private final boolean[][] BtmK =new boolean[1000][1000];
+  //  private Integer[][] BtmK =new Integer[1000][1000];
     private String BtmStr;
     public Stack<String> BtmStk = new Stack<>();
-    private int pos;
+
     public Bottom_stack_push(String title) {
         super(title);
     }
@@ -14,17 +14,51 @@ public class Bottom_stack_push extends btm{
     public String getBtmStr() {
         return BtmStr;
     }
-
+    
     public void setBtmStr(String BtmStr) {
         this.BtmStr = BtmStr;
     }
-    public void pushStr(char BtmStrPush,String BtmStr) {
-        if( BtmStrPush=='(' || BtmStrPush==')'){
+    public void pushStr(String BtmStrPush) {
+        String btmStr = getBtmStr();
+        if(BtmStrPush.equals("(")||BtmStrPush.equals(")")){
             BtmStr = BtmStr + BtmStrPush;
-            pos++;
         }
-        BtmStk.push(this.BtmStr);
+        BtmStk.push(BtmStrPush);
     }
-    public Boolean checkds
+
+
+    public int CkBks(String s) {
+        s=BtmStr;
+        if (s == null) {
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int maxLen = 0;
+        int accumulatedLen = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                if (stack.isEmpty()) {   //如果栈为空，没有左括号可以匹配
+                    accumulatedLen = 0;
+                } else {
+                    int matchedPos = stack.pop(); //从最近的'('作为起点
+                    int matchedLen = i - matchedPos + 1;	//计算两括号间的长度
+
+                    if (stack.isEmpty()) {			//如果栈为空，没有左括号可以匹配
+                        accumulatedLen += matchedLen;
+                        matchedLen = accumulatedLen;	//更新当前匹配括号序列长度
+                    } else {
+                        matchedLen = i - stack.peek();
+                    }
+                    maxLen = Math.max(maxLen, matchedLen);
+                }
+            }
+        }
+
+        return maxLen;
+}
 
 }
