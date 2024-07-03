cf1c68d5e63405f9e95d1b38a71b10b4d08abe0f
LieGeee <116617153+LieGeee@users.noreply.github.com>
2024-07-03 12:55:08 +0800

    完善判断逻辑.写了部分关于显示的定义

--- calculator/src/com/cyx/Bottom/PsplPlP.java
+++ calculator/src/com/cyx/Bottom/PsplPlP.java
@@ -1,9 +1,27 @@
 package com.cyx.Bottom;
-
+import java.util.LinkedList;
+import java.util.Queue;
 public class PsplPlP {
 
     //获取字符串,把字符串放到队列里,队列控制元素个数,
 
     private static final int PspSz=8;       //后续根据使用情况更改
     private static final int PspLs=18;      //后续根据需求更改
+    private static final int PLocX=121;          //开始的x坐标
+    private static final int PLocY=121;         //开始的y坐标
+
+    public static int getPLocX() {
+        return PLocX;
+    }
+    public static int getPLocY() {
+        return PLocY;
+    }
+    public static int getPspSz() {
+        return PspSz;
+    }
+    public static int getPspLs() {
+        return PspLs;
+    }
+
+
 }
--- calculator/src/com/cyx/Bottom/btm.java
+++ calculator/src/com/cyx/Bottom/btm.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.cyx.Bottom.Bottom_stack_push.BtmStr;
 
@@ -22,14 +24,14 @@ public class btm {
 
     public JButton[] MakeButton() {
         String[] button_names = {"M", "Π", "(", ")", "!", "退出", "%", "sin", "cos", "tan", "cot", "del", "sec", "7", "8",
-                "9", "时间", "日期", "csc", "4", "5", "6", "X", "/", "C", "1", "2", "3", "+", "-", "AC", "0", "00", ".", "="};//按钮内容
+                "9", "时间", "日期", "csc", "4", "5", "6", "X", "÷", "C", "1", "2", "3", "+", "-", "AC", "0", "00", ".", "="};//按钮内容
         JButton[] buttons = new JButton[button_names.length];
 
         for (int i = 0; i < button_names.length; i++) {
 
             JButton bt = new JButton(button_names[i]);//对象数组
             if(i!=button_names.length-1)
-            bt.setBounds(Bottom_Firx+(i%6)*Bottom_Movex,Bottom_Firy+i/6*Bottom_Movey,Bottom_Width,Bottom_height);
+                bt.setBounds(Bottom_Firx+(i%6)*Bottom_Movex,Bottom_Firy+i/6*Bottom_Movey,Bottom_Width,Bottom_height);
             else  bt.setBounds(Bottom_Firx+(i%6)*Bottom_Movex,Bottom_Firy+i/6*Bottom_Movey,Bottom_Width+150,Bottom_height);
             // bt.setPreferredSize(new Dimension(Bottom_Width, Bottom_height));
             Font font = new Font("宋体", Font.BOLD, 40); // 这里可以自定义字体、样式和大小
@@ -41,11 +43,36 @@ public class btm {
             bt.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
 
+                    Date date=new Date();  //输出时间
+
                     btm w=new btm();
                     w.BtmStrPush = e.getActionCommand();
-                    Bottom_stack_push a=new Bottom_stack_push();
-                    a.pushStr(BtmStr,w);
-                    System.out.println(BtmStr);
+                    if(w.BtmStrPush.equals("时间")){
+                        System.out.println(date);
+
+                    } else if (w.BtmStrPush.equals("日期")) {
+
+                        SimpleDateFormat dateFormat_min=new SimpleDateFormat();//设置当前时间的格式，为年-月-日 时-分-秒
+                        System.out.println(dateFormat_min.format(date));
+                        BtmStr="";
+                        //输出日期
+                    }else if(w.BtmStrPush.equals("X")){
+
+                        w.BtmStrPush="*";
+                        Bottom_stack_push a=new Bottom_stack_push();
+                        a.pushStr(BtmStr,w);
+                        System.out.println(BtmStr);
+                    }else if (w.BtmStrPush.equals("÷")){
+
+                        w.BtmStrPush="/";
+                        Bottom_stack_push a=new Bottom_stack_push();
+                        a.pushStr(BtmStr,w);
+                        System.out.println(BtmStr);
+                    }else if(w.BtmStrPush.equals("del")){
+
+                        BtmStr = BtmStr.substring(0, BtmStr.length() - 1);
+                    }
+
                 }
             });
             buttons[i]=bt;
@@ -54,4 +81,4 @@ public class btm {
         return buttons;
     }
 
-    }
+}
