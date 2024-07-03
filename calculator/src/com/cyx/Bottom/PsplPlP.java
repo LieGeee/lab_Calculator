0dae86958f63b8f43ff827083990fc30d250a332
LieGeee <116617153+LieGeee@users.noreply.github.com>
2024-07-03 12:31:19 +0800

    一点点关于显示的定义

---
+++ calculator/src/com/cyx/Bottom/PsplPlP.java
@@ -0,0 +1,9 @@
+package com.cyx.Bottom;
+
+public class PsplPlP {
+
+    //获取字符串,把字符串放到队列里,队列控制元素个数,
+
+    private static final int PspSz=8;       //后续根据使用情况更改
+    private static final int PspLs=18;      //后续根据需求更改
+}
